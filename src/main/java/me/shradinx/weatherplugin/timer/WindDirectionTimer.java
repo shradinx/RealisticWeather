42ff4f882fe7efc85ae9531bccdd2e3abbd2563e
Shradinx <luphcore@gmail.com>
2024-06-05 13:42:23 -0400

    add wind direction change timer

---
+++ src/main/java/me/shradinx/weatherplugin/timer/WindDirectionTimer.java
@@ -0,0 +1,26 @@
+package me.shradinx.weatherplugin.timer;
+
+import me.shradinx.weatherplugin.WeatherPlugin;
+import org.bukkit.World;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.util.Random;
+
+public class WindDirectionTimer extends BukkitRunnable {
+    private final World world;
+    private final Random random = new Random();
+    
+    public WindDirectionTimer(World world) {
+        this.world = world;
+    }
+    
+    @Override
+    public void run() {
+        if (world.getTime() != 6000L) return;
+        int x = random.nextInt(-1, 2);
+        int z = random.nextInt(-1, 2);
+        Vector vector = new Vector(x, 0, z);
+        WeatherPlugin.getPlugin().setWindDirection(vector);
+    }
+}
