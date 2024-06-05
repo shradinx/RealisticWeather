ec32ede7bc30a60ef69fb140b3a2301f55655594
Shradinx <luphcore@gmail.com>
2024-06-05 19:08:46 -0400

    fix LocationUtils#getRandomLocation()

--- src/main/java/me/shradinx/weatherplugin/utils/LocationUtils.java
+++ src/main/java/me/shradinx/weatherplugin/utils/LocationUtils.java
@@ -5,20 +5,13 @@ import org.bukkit.Location;
 import java.util.Random;
 
 public class LocationUtils {
-    public static Location getRandomLocation(Location origin, double radius, boolean is3D) {
-        Random random = new Random();
-        double randomRadius = random.nextDouble() * radius;
-        double theta = Math.toRadians(random.nextDouble() * 360);
-        double phi = Math.toRadians(random.nextDouble() * 180 - 90);
-        
-        double x = randomRadius * Math.cos(theta) * Math.sin(phi);
-        double y = randomRadius * Math.sin(theta) * Math.cos(phi);
-        double z = randomRadius * Math.cos(phi);
-        Location loc = origin.clone().add(x, origin.getY(), z);
-        if (is3D) {
-            loc.add(0, y, 0);
-        }
-        return loc;
+    public static Location getRandomLocation(Location origin, int radius) {
+       Random random = new Random();
+       int randomX = (random.nextBoolean() ? 1 : -1) * random.nextInt(0, radius + 1);
+       int randomY = random.nextInt(3, 7);
+       int randomZ = (random.nextBoolean() ? 1 : -1) * random.nextInt(0, (radius + 1));
+       
+       return origin.clone().add(randomX, randomY, randomZ);
     }
     
 }
