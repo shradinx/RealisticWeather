f94765d559034ac6932fb47bd5a87d1d6b703511
Shradinx <luphcore@gmail.com>
2024-06-09 01:08:33 -0400

    add biome specific wind particles

--- src/main/java/me/shradinx/realisticweather/timer/WindParticleEffectTimer.java
+++ src/main/java/me/shradinx/realisticweather/timer/WindParticleEffectTimer.java
@@ -5,9 +5,11 @@ import me.shradinx.realisticweather.RealisticWeather;
 import me.shradinx.realisticweather.utils.LocationUtils;
 import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
@@ -15,10 +17,16 @@ public class WindParticleEffectTimer extends BukkitRunnable {
     
     private final Player player;
     private final Particle particle;
+    private Color color = null;
+    private ItemStack stack = null;
     
     public WindParticleEffectTimer(Player player, Particle particle) {
         this.player = player;
         this.particle = particle;
+        switch (particle) {
+            case ENTITY_EFFECT -> color = Color.fromRGB(245, 245, 245);
+            case ITEM -> stack = new ItemStack(Material.SAND);
+        }
     }
     
     @Override
@@ -27,33 +35,44 @@ public class WindParticleEffectTimer extends BukkitRunnable {
         if (direction.isZero()) return;
         direction = direction.clone().multiply(0.25);
         Location origin = player.getLocation().clone();
+        
         for (int i = 0; i < 10; i++) {
-            Location randomLoc = LocationUtils.getRandomLocation(origin, 15);
+            Location randomLoc = LocationUtils.getRandomLocation(origin, 30);
             Vector finalDirection = direction;
+            
             new BukkitRunnable() {
                 double counter = 0;
                 @Override
                 public void run() {
-                    if (counter > 200) {
+                    if (counter > 400) {
                         this.cancel();
                         return;
                     }
                     counter++;
+                    
                     Location newLoc = randomLoc.add(finalDirection);
                     Block block = newLoc.getBlock();
+                    
                     if (block.isCollidable()) {
                         this.cancel();
                         return;
                     }
-                    new ParticleBuilder(particle)
+                    
+                    ParticleBuilder builder = new ParticleBuilder(particle)
                         .location(newLoc)
                         .count(10)
                         .receivers(player)
-                        .data(Color.fromRGB(245, 245, 245))
-                        .extra(0)
-                        .spawn();
+                        .extra(0);
+                    
+                    if (particle.equals(Particle.ENTITY_EFFECT)) {
+                        builder.data(color);
+                    } else if (particle.equals(Particle.ITEM)) {
+                        builder.data(stack);
+                    }
+                    
+                    builder.spawn();
                 }
-            }.runTaskTimer(RealisticWeather.getPlugin(), 0, 5);
+            }.runTaskTimer(RealisticWeather.getPlugin(), 0, 2);
         }
     }
 }
