f5ac4f6682aff7105502f88aba44f2d5ac354349
Shradinx <luphcore@gmail.com>
2024-07-08 20:09:57 -0400

    make dropped items affected by wind

--- src/main/java/me/shradinx/realisticweather/listener/ThrowableListener.java
+++ src/main/java/me/shradinx/realisticweather/listener/ThrowableListener.java
@@ -1,45 +1,51 @@
 package me.shradinx.realisticweather.listener;
 
 import me.shradinx.realisticweather.RealisticWeather;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
-import org.bukkit.projectiles.ProjectileSource;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.util.Vector;
 
-public class ProjectileListener implements Listener {
+public class ThrowableListener implements Listener {
     
     private final RealisticWeather plugin;
     
-    public ProjectileListener(RealisticWeather plugin) {
+    public ThrowableListener(RealisticWeather plugin) {
         this.plugin = plugin;
     }
     
     @EventHandler
     public void onProjectileLaunch(ProjectileLaunchEvent event) {
         if (event.isCancelled()) return;
+        setVelocity(event.getEntity());
+    }
+    
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+        if (event.isCancelled()) return;
+        setVelocity(event.getItemDrop());
+    }
+    
+    private void setVelocity(Entity throwable) {
         if (!plugin.getConfig().getBoolean("wind-enabled")) return;
         if (!plugin.getConfig().getBoolean("wind-affect-projectiles")) return;
-        Projectile projectile = event.getEntity();
         
-        Vector direction = projectile.getVelocity();
+        Vector direction = throwable.getVelocity();
         Vector windDirection = plugin.getWindDirection();
         if (windDirection.isZero()) return;
         
         double dot = direction.dot(windDirection);
         Vector midpoint = direction.getMidpoint(windDirection);
         
-        /*
-        plugin.getLogger().info("Wind Direction: " + windDirection);
-        plugin.getLogger().info("Dot Product: " + dot);
-         */
-        
+        double multiplier = 0.75;
         if (dot <= 0) {
-            projectile.setVelocity(direction.midpoint(midpoint).multiply(0.75));
+            throwable.setVelocity(direction.midpoint(midpoint).multiply(multiplier));
         } else {
-            projectile.setVelocity(midpoint);
+            throwable.setVelocity(midpoint);
         }
     }
 }
